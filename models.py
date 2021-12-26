63febbd6962a4a5f9005e4a0420ef4ed5a782b24
discolarrence <discolarrence@gmail.com>
2021-12-25 19:06:19 -0500

    added csv

--- models.py
+++ models.py
@@ -1,21 +1,18 @@
-from sqlalchemy import create_engine, Column, Integer, String, Date, engine, interfaces
+from sqlalchemy import create_engine, Column, Integer, String, Date
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.sql.elements import collate
-from sqlalchemy.sql.expression import column
 
-
-engine = create_engine('sqlite:///boks.db', echo = False)
+engine = create_engine('sqlite:///books.db', echo = False)
 Session = sessionmaker(bind=engine)
 session = Session()
-Dase = declarative_base()
+Base = declarative_base()
 
 
 class Book(Base):
     __tablename__ = 'books'
 
     id = Column(Integer, primary_key=True)
-    title = Column('Title'. String)
+    title = Column('Title', String)
     author = Column('Author', String)
     published_date = Column('Published', Date)
     price = Column('Price', Integer)
